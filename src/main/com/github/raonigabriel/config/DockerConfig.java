6489683cdc92299663ffbd2a239b56cb39279b3e
Raoni Meira Gabriel <2515360+raonigabriel@users.noreply.github.com>
2020-09-15 16:30:00 -0300

    Create DockerConfig.java

---
+++ src/main/com/github/raonigabriel/config/DockerConfig.java
@@ -0,0 +1,34 @@
+package com.github.raonigabriel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.transport.DockerHttpClient;
+import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+
+@Configuration
+public class DockerConfig {
+
+	@Bean
+	public DockerClientConfig dockerClientConfig() {
+		return DefaultDockerClientConfig.createDefaultConfigBuilder()
+			    .withDockerHost("tcp://localhost:2735")
+			    .withDockerTlsVerify(false).build();
+	}
+	
+	@Bean
+	public DockerHttpClient dockerHttpClient(DockerClientConfig dockerClientConfig) {
+		return new ZerodepDockerHttpClient.Builder()
+				.dockerHost(dockerClientConfig.getDockerHost()).build();
+	}
+	
+	@Bean
+	public DockerClient dockerClient(DockerClientConfig config, DockerHttpClient transport) {
+		return DockerClientImpl.getInstance(config, transport);
+	}
+
+}
