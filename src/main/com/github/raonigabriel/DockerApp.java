597cd8c75bdb7cc1ef553f6de73696971cab9366
Raoni Meira Gabriel <2515360+raonigabriel@users.noreply.github.com>
2020-09-15 16:29:26 -0300

    Create DockerApp.java

---
+++ src/main/com/github/raonigabriel/DockerApp.java
@@ -0,0 +1,13 @@
+package com.github.raonigabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApp.class, args);
+	}
+
+}
